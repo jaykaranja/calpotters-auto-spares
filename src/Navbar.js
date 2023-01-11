3a75567d2fd8f8c48ab739159ed81c706c5cc4f4
krakh3ad <mainakaranja2001@gmail.com>
2023-01-12 01:16:32 +0300

    Stacking features

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div
+    className='
+      h-12
+      bg-red-700
+
+    '
+    >
+      <div className='
+        ml-5;
+      '>
+        Hello
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
